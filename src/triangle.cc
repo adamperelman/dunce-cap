247774e13b48fadd7c491fd7fde621d30c4ca727
Adam Perelman <adamp14@stanford.edu>
2015-01-23 11:30:33 -0800

    make triangle query accept filename for data

--- src/triangle.cc
+++ src/triangle.cc
@@ -6,11 +6,17 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cout << "Usage: ./triangle.exe <filename>" << endl;
+    exit(0);
+  }
+
+
   Database db;
-  db.AddRelation(new Relation("data/facebook_short.txt", "R", {"a", "b"}));
-  db.AddRelation(new Relation("data/facebook_short.txt", "S", {"b", "c"}));
-  db.AddRelation(new Relation("data/facebook_short.txt", "T", {"c", "a"}));
+  db.AddRelation(new Relation(argv[1], "R", {"a", "b"}));
+  db.AddRelation(new Relation(argv[1], "S", {"b", "c"}));
+  db.AddRelation(new Relation(argv[1], "T", {"c", "a"}));
 
   Relation* result = db.GenericJoin({"R", "S", "T"});
 
