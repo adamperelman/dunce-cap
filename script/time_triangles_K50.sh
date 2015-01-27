a911bcaa79611f74123c304f039bf7028d4f1b7e
Susan Tu <susanctu@gmail.com>
2015-01-26 17:43:54 -0800

    timing script

---
+++ script/time_triangles_K50.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+for i in `seq 60 10 100`;
+do
+  rm -f ../data/random_connect0.8.txt
+  python random_graph.py $i 0.8 > ../data/random_connect0.8.txt
+  echo $i
+  time ../triangle.exe ../data/random_connect0.8.txt
+done
+
+
+
