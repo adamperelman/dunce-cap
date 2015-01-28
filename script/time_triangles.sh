d0e988c6704b5f44f9602f7573c52fd5b3175414
Adam Perelman <adamperelman@gmail.com>
2015-01-27 19:46:44 -0800

    ignore generated files

--- script/time_triangles.sh
+++ script/time_triangles.sh
@@ -2,8 +2,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 for i in `seq 130 10 150`;
 do
-  rm -f ../data/random_connect0.8.txt
-  python random_graph.py $i 0.8 > ../data/random_connect0.8.txt
+  rm -f ../data/GENERATED_random_connect0.8.txt
+  python random_graph.py $i 0.8 > ../data/GENERATED_random_connect0.8.txt
   echo $i
-  time ../triangle.exe ../data/random_connect0.8.txt
+  time ../triangle.exe ../data/GENERATED_random_connect0.8.txt
 done
