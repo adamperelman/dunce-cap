638d7b42445ba9ea45a8e6fb82afb44cb3e004df
Susan Tu <susanctu@gmail.com>
2015-02-02 16:46:38 -0800

    test for relations with more attrs

--- test/relation_test.cc
+++ test/relation_test.cc
@@ -14,3 +14,14 @@ TEST_CASE("Relations are created correctly") {
   REQUIRE(r.contains({2, 4}));
   REQUIRE(!r.contains({3,2}));
 }
+
+TEST_CASE("Make sure we can create a relation with 3 attrs") {
+  Relation r("data/triples.txt", "triples", {"a", "b", "c"});
+  REQUIRE(r.size() == 3);
+  REQUIRE(r.contains({1, 2, 3}));
+  REQUIRE(r.contains({4, 5, 6}));
+  REQUIRE(r.contains({7, 8, 9}));
+  REQUIRE(!r.contains({1, 2, 6}));
+  vector<string> expected_attrs({"a", "b", "c"});
+  REQUIRE(r.attrs() == expected_attrs);
+}
