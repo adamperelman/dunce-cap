eaece2fb6032d6b72e7a3a879d1d83bc0f020e7b
Adam Perelman <adamperelman@gmail.com>
2015-02-05 13:53:57 -0800

    make triangle script print sttments less annoying

--- src/triangle.cc
+++ src/triangle.cc
@@ -15,22 +15,26 @@ int main(int argc, char* argv[]) {
 
 
   Database db;
+
+  cout << "loading database..." << endl;
   db.AddRelation(new Relation(argv[1], "R", {"a", "b"}));
   db.AddRelation(new Relation(argv[1], "S", {"b", "c"}));
   db.AddRelation(new Relation(argv[1], "T", {"c", "a"}));
 
-  cout << "loaded database: " << endl;
-  cout << db << endl;
+  cout << "loaded database" << endl;
 
+  cout << "performing join..." << endl;
   typedef chrono::high_resolution_clock Clock;
   typedef chrono::milliseconds ms;
+
   Clock::time_point start_join = Clock::now();
   Relation* result = db.GenericJoin({"R", "S", "T"});
-
   Clock::time_point end_join = Clock::now();
+
+  cout << "done performing join" << endl;
   ms join_time = chrono::duration_cast<ms>(end_join - start_join);
+
   cout << join_time.count() << "ms\n" << endl;
-//  cout << (*result) << endl;
   cout << "size: " << result->size() << endl;
 
   delete result;
